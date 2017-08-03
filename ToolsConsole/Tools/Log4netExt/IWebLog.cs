340683ebaad7dcedf94f6c5e720b453e5bf35db7
余作想 <963526232@qq.com>
2017-08-03 18:08:25 +0800

    123

---
+++ ToolsConsole/Tools/Log4netExt/IWebLog.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using System;
+
+namespace Tools.Log4netExt
+{
+    public interface IWebLog : ILog
+    {
+        void Info(string clientIP, string requestUrl, object message, int loginId = 0, string loginUserName = "");
+        void Info(string clientIP, string requestUrl, object message, Exception t, int loginId = 0, string loginUserName = "");
+
+        void Warn(string clientIP, string requestUrl, object message, int loginId = 0, string loginUserName = "");
+        void Warn(string clientIP, string requestUrl, object message, Exception t, int loginId = 0, string loginUserName = "");
+
+        void Error(string clientIP, string requestUrl, object message, int loginId = 0, string loginUserName = "");
+        void Error(string clientIP, string requestUrl, object message, Exception t, int loginId = 0, string loginUserName = "");
+
+        void Fatal(string clientIP, string requestUrl, object message, int loginId = 0, string loginUserName = "");
+        void Fatal(string clientIP, string requestUrl, object message, Exception t, int loginId = 0, string loginUserName = "");
+    }
+}
